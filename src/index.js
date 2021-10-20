8d93a95375b0ee9b1d69ce3dfac02dc36e9cbf7b
AZARIA <allysaidi64@gmail.com>
2021-10-20 17:18:27 +0200

    Add header

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 // import _ from 'lodash';
 // import './style.css';
+import logo from './images/logo.png';
+
+const img = document.querySelector('.logo-image');
+// img.src = logo;
+img.setAttribute('src', logo);
