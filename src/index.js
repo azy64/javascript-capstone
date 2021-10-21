04299a9785dd6c89a83271de67e42f54141768c6
Kingstalux <kingsleyngu25@gmail.com>
2021-10-21 13:07:51 +0100

    Add post and get comment functions

--- src/index.js
+++ src/index.js
@@ -17,6 +17,46 @@ const init = {
 img.setAttribute('src', logo);
 const limit = { inf: 0, sup: 50 };
 
+
+/**
+ * this function fetch data from the API comment endpoint
+ */
+const getComments = async (id) => {
+  const response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/YG7f4fmyaSRAJHzw8A5N/comments?item_id=' + id);
+  const json = await response.json();
+  console.log(json);
+};
+
+  /**
+ * Display data about a particular series
+ */    
+   const displayInfo = (data) => {
+    const template = `
+    <div class="serie" id="${data.id}">
+      <div class="serie-image">
+        <img src="${data.image.original}">
+      </div>
+
+      <div>
+       <p class="serie-title p">
+        Name: ${data.name}
+       </p>
+       <p>
+        Rating: ${data.rating.average}
+       </p>
+       <p class="serie-description p">
+        ${data.summary}
+       </p>
+      </div>
+</div>
+    
+    `;
+    info.innerHTML += template;
+  }
+
+/**
+ * Event for implementing comment popup
+ */
 const addButtonListen = () => {
   const buttons = document.querySelectorAll('.btn')
   buttons.forEach(btn => {
@@ -24,41 +64,54 @@ const addButtonListen = () => {
       document.querySelector('.container').classList.add('hide')
       document.querySelector('#header').classList.add('hide')
       document.querySelector('.comment-section').classList.remove('hide')
-
-      const displayComments = (data) => {
-        const template = `
-        <div class="serie" id="${data.id}">
-          <div class="serie-image">
-            <img src="${data.image.original}">
-          </div>
-
-          <div>
-           <p class="serie-title p">
-            Name: ${data.name}
-           </p>
-           <p>
-            Rating: ${data.rating.average}
-           </p>
-           <p class="serie-description p">
-            ${data.summary}
-           </p>
-          </div>
-    </div>
-        
-        `;
-        info.innerHTML += template;
-      }
-
       const id = btn.parentElement.parentElement.id
 
+/**
+ * Get information about series to display
+ */
       fetch('https://api.tvmaze.com/shows')
       .then((resp) => resp.json())
       .then((datum) => {
         console.log(datum[id-1]);
-        displayComments(datum[id-1])
-      });     
+        displayInfo(datum[id-1])
+        
+      });
+      getComments(id)
+      
+/**
+ * Function to post comments to API
+ */
+document.querySelector('.button-submit').addEventListener('click', (e) => {
+  e.preventDefault()
+  const name = document.querySelector('.name').value
+  const message = document.querySelector('#message').value
+  
+  const postComments = async () => {
+    const response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/YG7f4fmyaSRAJHzw8A5N/comments', {
+      method: 'post',
+      body: JSON.stringify({
+        "item_id": id,
+        "username": name,
+        "comment": message
+    }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+    const json = await response.text();
+    console.log(json);
+
+    getComments(id)
+  }
+
+  document.querySelector('.name').value = ''
+  document.querySelector('#message').value = ''
+  postComments()
+})
     })
   }); 
+
+  
 }
 
 /**
@@ -130,3 +183,12 @@ window.addEventListener('scroll', () => {
     loadData();
   }
 });
+
+
+
+
+
+/**
+ * Unique ID generated for App
+ */
+// YG7f4fmyaSRAJHzw8A5N
